b61769fd4a303d79c99831143a4ba80311b6c678
Vishal <e23ai045@shanmugha.edu.in>
2025-04-13 21:46:57 +0530

    footer updation

--- core/resources/views/layouts/footer.blade.php
+++ core/resources/views/layouts/footer.blade.php
@@ -3,7 +3,16 @@
   <head>
    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    
+    <style>
+      .pd{
+        padding:0px 5px !important;
+      }
+      .h:hover{
+        transform: translateY(-3px);
+        transition-duration: 0.2s;
+      }
+    </style>
+
   </head>
   <body>
      
@@ -14,6 +23,38 @@
           <div class="row">
             <div class="col-lg-4 width-20">
               <div class="mb-10"><img src="{{ asset('assets/yc/imgs/yclogo-white.svg') }}" alt="yc"></div>
+              <div class="col-lg-12 col-md-12 mb-3 mb-lg-0 text-center mt-3 d-flex justify-content-start align-items-center gap-2 row">
+                <h6 class="color-white mb-1 ml-45 text-lg-start mr-2 col-lg-6 col-md-6 col-6">Follow Us</h6>
+                <div class="rounded d-flex justify-content-center col-lg-12 col-md-6 col-6 ml-2 align-items-center p-2">
+                  <a class="h mx-2 color-white" href="#">
+                  <svg fill="blue" width="20" height="20" viewBox="0 0 24 22">
+                  <path fill="white" d="M22.675 0h-21.35C.592 0 0 .593 0 1.326v21.348C0 23.407.592 24 1.325 
+                    24H12.82v-9.294H9.692V11.01h3.128V8.413c0-3.1 1.894-4.788 4.659-4.788 
+                    1.325 0 2.463.099 2.794.143v3.24l-1.918.001c-1.504 0-1.796.715-1.796 
+                    1.763v2.31h3.587l-.467 3.696h-3.12V24h6.116c.73 0 1.324-.593 
+                    1.324-1.326V1.326C24 .593 23.407 0 22.675 0z"/>
+                  </svg>
+                  </a>
+                  <a class="h mx-2" href="#">
+                    <svg fill="blue" width="20" height="20" viewBox="0 0 22 22">
+                    <path fill="white" d="M14.5 10.6 23.5 0h-2.2L13.3 9.3 6.6 0H0l9.4 
+                    12.9L0 24h2.2l8.4-9.5L17.4 24H24l-9.5-13.4z"/>
+                    </svg>
+                  </a>
+                  <a class="h mx-2" href="#">
+                    <svg fill="blue" width="24" height="26" viewBox="0 0 24 26">
+                    <path fill="white" d="M7.75 2h8.5C20.097 2 22 3.903 22 7.75v8.5C22 20.097 20.097 22 16.25 22h-8.5C3.903 22 2 20.097 2 16.25v-8.5C2 3.903 3.903 2 7.75 2zm0 1.5C5.679 3.5 4.5 4.679 4.5 7.75v8.5c0 3.071 1.179 4.25 3.25 4.25h8.5c3.071 0 4.25-1.179 4.25-4.25v-8.5c0-3.071-1.179-4.25-4.25-4.25h-8.5zm4.25 4a5.75 5.75 0 1 1 0 11.5 5.75 5.75 0 0 1 0-11.5zm0 1.5a4.25 4.25 0 1 0 0 8.5 4.25 4.25 0 0 0 0-8.5zm5.5-1.25a1.25 1.25 0 1 1-2.5 0 1.25 1.25 0 0 1 2.5 0z"/>
+
+                    </svg>
+                  </a>
+                  <a class="h mb-1 mx-2" href="#">
+                    <svg fill="blue" width="24" height="26" viewBox="0 0 24 26">
+                    <path fill="white" d="M4.98 3.5C4.98 4.88 3.87 6 2.49 6S0 4.88 0 3.5 1.11 1 2.49 1 4.98 2.12 4.98 3.5zM.53 8.5h3.93V24H.53V8.5zM8.49 8.5h3.77v2.1h.05c.53-.95 1.83-1.95 3.77-1.95 4.03 0 4.77 2.65 4.77 6.1V24h-4.01v-7.6c0-1.8-.03-4.1-2.5-4.1-2.5 0-2.89 1.95-2.89 3.96V24H8.49V8.5z"/>
+
+                    </svg>
+                  </a>
+                </div>
+              </div>
             </div>
             
             <div class="col-lg- width-16 mb-30 ml-100">
@@ -35,54 +76,61 @@
                 <li><a href="{{route('video-series')}}"><p class="color-white"><p class="color-white">Infographics</p></a></li>
               </ul>
             </DIV>
-            <div class="col-lg-4 width-16 mb-30 ml-100 ">
-            <h5 class="mb-10 color-white">Contact us</h5>
-            <div class="d-flex align-items-start mb-2 color-white"><strong>
-              <i class="bi bi-geo-alt me-3 mt-1"></i><strong>
-              <p class="font-md mb-0 color-white">
-                West Car Street, Tiruchengode,<br class="d-none d-lg-block"> Tamil Nadu, India.
-              </p>
+            <!-- <div class="col-lg-4 width-16 mb-30 ml-100 ">
+              <h5 class="mb-10 color-white">Contact us</h5>
+              <div class="d-flex align-items-start mb-2 color-white"><strong>
+                <i class="bi bi-geo-alt me-2 mt-1"></i><strong>
+                <p class="font-md mb-0 color-white">
+                  West Car Street, Tiruchengode,<br class="d-none d-lg-block"> Tamil Nadu, India.
+                </p>
+              </div>
+                <div class="font-md  color-white"><strong class="font-md-bold"><i class="bi bi-telephone me-2"></i></strong>+91-9647555066</div>
+                <div class="font-md  color-white"><strong class="font-md-bold"></strong><i class="bi bi-envelope me-2"></i>info@younngchanakya.com</div>
+              </div>
             </div>
-              <div class="font-md  color-white"><strong class="font-md-bold"><i class="bi bi-telephone"></i></strong>+91-9647555066</div>
-              <div class="font-md  color-white"><strong class="font-md-bold"></strong><i class="bi bi-envelope"></i>info@younngchanakya.com</div>
-          </div>
-        </div>
 
-      </div>
+          </div> -->
+          <div class="col-lg-4 width-16 mb-30 ml-100">
+  <h5 class="mb-10 color-white">Contact us</h5>
+
+  <!-- Address (click to open location in Google Maps) -->
+  <a href="https://www.google.com/maps?q=West+Car+Street,+Tiruchengode,+Tamil+Nadu,+India"
+     target="_blank"
+     class="d-flex align-items-start font-md color-white mb-2 text-decoration-none">
+    <i class="bi bi-geo-alt me-2 mt-1"></i>
+    <span>West Car Street, Tiruchengode,<br class="d-none d-lg-block"> Tamil Nadu, India.</span>
+  </a>
+
+  <!-- Phone (click to call) -->
+  <a href="tel:+919647555066" class="d-flex align-items-center font-md color-white mb-2 text-decoration-none">
+    <i class="bi bi-telephone me-2"></i>
+    <span>+91-9647555066</span>
+  </a>
+
+  <!-- Email (click to send email) -->
+  <a href="mailto:info@younngchanakya.com" class="d-flex align-items-center font-md color-white mb-3 text-decoration-none">
+    <i class="bi bi-envelope me-2"></i>
+    <span>info@youngchanakya.com</span>
+  </a>
+
 
       </div>
       <div class="footer-2">
-        <div class="container">
-          <div class="footer-bottom">
-            <div class="row">
-              <div class="col-lg-6 col-md-12 text-center text-lg-start">
-                <ul class="menu-bottom">
-                  <li><a class="font-sm color-white" href="term-conditions.html">Privacy policy</a></li>
-                  <li><a class="font-sm color-white" href="term-conditions.html">Cookies</a></li>
-                  <li><a class="font-sm color-white" href="term-conditions.html">Terms of service</a></li>
-                </ul>
-              </div>
-              <div class="col-lg-6 col-md-12 text-center text-lg-end"><span class="color-white font-md">©YC Official 2023. All right reversed.</span></div>
-            </div>
   <div class="container">
     <div class="footer-bottom py-4">
       <div class="row align-items-center text-center text-lg-start">
         
         <!-- Column 1: Copyright -->
         <div class="col-lg-4 col-md-12 mb-3 mb-lg-0">
-          <span class="color-white font-md">Copyrights© 2025. All rights reserved.</span>
+          <span class="color-white font-md">Copyright © 2025 Young Chanakya. All rights reserved.</span>
         </div>
         
         <!-- Column 2: Follow Us -->
-        <div class="col-lg-4 col-md-12  gap-3 mb-3 mb-lg-0 text-center mt-3 d-flex justify-content-center align-items-center column">
-          <h6 class="color-white mb-2 text-lg-end mr-2 col-lg-6 col-md-6 col-6">Follow Us</h6>
-          <div class="bg-white rounded d-flex justify-content-center col-lg-6 col-md-6 col-6 ml-2 align-items-center p-2">
-            <a class="icon-socials icon-facebook mx-2" href="#"></a>
-            <a class="icon-socials icon-instagram mx-2" href="#"></a>
-            <a class="icon-socials icon-twitter mx-2" href="#"></a>
-            <a class="icon-socials icon-linkedin mx-2" href="#"></a>
-            <a class="icon-socials icon-youtube mx-2" href="#"></a>
-          </div>
+        <div class="col-lg-4 col-md-12 text-lg-center">
+          <ul class="menu-bottom list-inline mb-0">
+            <li class="list-inline-item"><a class="font-sm color-white" href="term-conditions.html">Privacy Policy</a></li>
+            <li class="list-inline-item"><a class="font-sm color-white" href="term-conditions.html">Terms of Service</a></li>
+          </ul>
         </div>
 
 
@@ -90,8 +138,8 @@
         <!-- Column 3: Bottom Links -->
         <div class="col-lg-4 col-md-12 text-lg-end">
           <ul class="menu-bottom list-inline mb-0">
-            <li class="list-inline-item"><a class="font-sm color-white" href="term-conditions.html">Privacy Policy</a></li>
-            <li class="list-inline-item"><a class="font-sm color-white" href="term-conditions.html">Terms of Service</a></li>
+            <li class="list-inline-item"><strong class="font-sm color-white">Partners : </strong></li>
+            <li class="list-inline-item"><ul class="list-inline"><li><a class="font-sm color-white pd" href="https://fintechgie.com/">FintechGie </a></li><li class=" font-sm color-white"> & </li><li><a class="font-sm color-white pd" href="https://wegeni.com/"> WeGeni</a></ul></li></li> 
           </ul>
         </div>
 
