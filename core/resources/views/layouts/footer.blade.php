9ee0db85370d3ecab00511089ee250d2961c90f3
Vishal <e23ai045@shanmugha.edu.in>
2025-04-13 17:41:21 +0530

    footer updated

--- core/resources/views/layouts/footer.blade.php
+++ core/resources/views/layouts/footer.blade.php
@@ -1,60 +1,93 @@
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+   
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    
+  </head>
   <body>
      
     <footer class="footer" style="background-color:#024430;">
       <div class="footer-1" style="background-color:#024430;">
-        <div class="container">
+      <div class="container">
+      <div class="container">
           <div class="row">
             <div class="col-lg-4 width-20">
-              <div class="mb-10"><img src="{{ asset('assets/yc/imgs/yc-logof.png') }}" alt="yc"></div>
-              <p class="font-md mb-20 color-white">West Car Street, Tiruchengode,<br class="d-none d-lg-block"> Tamil Nadu, India.</p>
-              <div class="font-md  color-white"><strong class="font-md-bold"><i class="bi bi-telephone"></i></strong>+91-9647555066</div>
-              <div class="font-md  color-white"><strong class="font-md-bold"></strong><i class="bi bi-envelope"></i>info@younngchanakya.com</div>
-              <h6 class="color-white">Follow Us</h6>
-              <div class="mt-15"><a class="icon-socials icon-facebook" href="#"></a><a class="icon-socials icon-instagram" href="#"></a><a class="icon-socials icon-twitter" href="#"></a><a class="icon-socials icon-linkedin" href="#"></a><a class="icon-socials icon-youtube" href="#"></a></div>
+              <div class="mb-10"><img src="{{ asset('assets/yc/imgs/yclogo-white.svg') }}" alt="yc"></div>
             </div>
-            <div class="col-lg-4 width-20 mb-30 ml-100 ">
-            <h5 class="mb-10 color-white">Company Info</h5>
-              <ul class="menu-footer">
-                <li><a href="{{route('about')}}"><p class="color-white">About us</p></a></li>
-                <li><a href="{{route('about-chanakya')}}"><p class="color-white">About Chanakya</p></a></li>
-                <li><a href="{{route('membership')}}"><p class="color-white">Membership</p></a></li>
-                <li><a href="{{route('program')}}"><p class="color-white">Program</p></a></li>
-                <li><a href="{{route('events')}}"><p class="color-white">Events</p></a></li>
-
-              </ul>
-            </div>
-            <div class="col-lg-4 width-16 mb-30 ml-100">
+            
+            <div class="col-lg- width-16 mb-30 ml-100">
             <h5 class="mb-10 color-white">Useful Links</h5>
               <ul class="menu-footer">
+                <li><a href="{{route('about-chanakya')}}"><p class="color-white">About Chanakya's</p></a></li>
                 <li><a href="{{route('become-a-chanakya')}}"><p class="color-white">Become a Chanakya</p></a></li>
                 <li><a href="{{route('become-a-sponsor')}}"><p class="color-white">Become a Sponsor</p></a></li>
-                <li><a href=""><p class="color-white">Our Chanakya's</p></a></li>
-                <li><a href="{{route('podcast')}}"><p class="color-white">Podcast</p></a></li>
-                <li><a href="{{route('blog')}}"><p class="color-white">Insights & Blogs</p></a></li>
+                <li><a href="{{route('career')}}"><p class="color-white">Career & Internships</p></a></li>
               </ul>
             </div>
-        
+            <div class="col-lg-2 width-16 mb-30 ml-100">
+            <h5 class="mb-10 color-white">Resources</h5>
+              <ul class="menu-footer">
+                <li><a href="{{route('podcast')}}"><p class="color-white">Podcast</p></a></li>
+                <li><a href="{{route('blog')}}"><p class="color-white">Blog</p></a></li>
+                <li><a href="{{route('case-studies')}}"><p class="color-white">Case Study</p></a></li>
+                <li><a href="{{route('video-series')}}"><p class="color-white"><p class="color-white">Video Series</p></a></li>
+                <li><a href="{{route('video-series')}}"><p class="color-white"><p class="color-white">Infographics</p></a></li>
+              </ul>
+            </DIV>
+            <div class="col-lg-4 width-16 mb-30 ml-100 ">
+            <h5 class="mb-10 color-white">Contact us</h5>
+            <div class="d-flex align-items-start mb-2 color-white"><strong>
+              <i class="bi bi-geo-alt me-3 mt-1"></i><strong>
+              <p class="font-md mb-0 color-white">
+                West Car Street, Tiruchengode,<br class="d-none d-lg-block"> Tamil Nadu, India.
+              </p>
+            </div>
+              <div class="font-md  color-white"><strong class="font-md-bold"><i class="bi bi-telephone"></i></strong>+91-9647555066</div>
+              <div class="font-md  color-white"><strong class="font-md-bold"></strong><i class="bi bi-envelope"></i>info@younngchanakya.com</div>
           </div>
         </div>
+
+      </div>
+
       </div>
       <div class="footer-2">
-        <div class="container">
-          <div class="footer-bottom">
-            <div class="row">
-              <div class="col-lg-6 col-md-12 text-center text-lg-start">
-                <ul class="menu-bottom">
-                  <li><a class="font-sm color-white" href="term-conditions.html">Privacy policy</a></li>
-                  <li><a class="font-sm color-white" href="term-conditions.html">Cookies</a></li>
-                  <li><a class="font-sm color-white" href="term-conditions.html">Terms of service</a></li>
-                </ul>
-              </div>
-              <div class="col-lg-6 col-md-12 text-center text-lg-end"><span class="color-white font-md">©Iori Official 2023. All right reversed.</span></div>
-            </div>
+  <div class="container">
+    <div class="footer-bottom py-4">
+      <div class="row align-items-center text-center text-lg-start">
+        
+        <!-- Column 1: Copyright -->
+        <div class="col-lg-4 col-md-12 mb-3 mb-lg-0">
+          <span class="color-white font-md">Copyrights© 2025. All rights reserved.</span>
+        </div>
+        
+        <!-- Column 2: Follow Us -->
+        <div class="col-lg-4 col-md-12  gap-3 mb-3 mb-lg-0 text-center mt-3 d-flex justify-content-center align-items-center column">
+          <h6 class="color-white mb-2 text-lg-end mr-2 col-lg-6 col-md-6 col-6">Follow Us</h6>
+          <div class="bg-white rounded d-flex justify-content-center col-lg-6 col-md-6 col-6 ml-2 align-items-center p-2">
+            <a class="icon-socials icon-facebook mx-2" href="#"></a>
+            <a class="icon-socials icon-instagram mx-2" href="#"></a>
+            <a class="icon-socials icon-twitter mx-2" href="#"></a>
+            <a class="icon-socials icon-linkedin mx-2" href="#"></a>
+            <a class="icon-socials icon-youtube mx-2" href="#"></a>
           </div>
         </div>
+
+
+        
+        <!-- Column 3: Bottom Links -->
+        <div class="col-lg-4 col-md-12 text-lg-end">
+          <ul class="menu-bottom list-inline mb-0">
+            <li class="list-inline-item"><a class="font-sm color-white" href="term-conditions.html">Privacy Policy</a></li>
+            <li class="list-inline-item"><a class="font-sm color-white" href="term-conditions.html">Terms of Service</a></li>
+          </ul>
+        </div>
+
       </div>
+    </div>
+  </div>
+</div>
+
     </footer>
     
   </body>
