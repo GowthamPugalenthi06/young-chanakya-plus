91bec8a7847452de55df552f8dc549202b03d1fc
Vishal6374 <e23ai045@shanmugha.edu.in>
2025-05-01 17:45:22 +0530

    video-series play button

--- core/resources/views/yc/video-series.blade.php
+++ core/resources/views/yc/video-series.blade.php
@@ -66,104 +66,166 @@
     <div class="row justify-content-center g-4">
 
       
-      <div class="col-md-6 col-lg-5">
-        <div class="card shadow rounded-4 overflow-hidden border-0 h-100 d-flex flex-column">
-          <!-- Thumbnail with Play Icon -->
-          <div class="position-relative" style="height: 200px; background-image: url('{{ asset('assets/yc/imgs/page/homepage1/event-09.jpg') }}'); background-size: cover; background-position: center;">
-            <a href="#" class="position-absolute top-50 start-50 translate-middle text-white text-decoration-none">
-              <i class="bi bi-play-circle-fill" style="font-size: 4rem;"></i>
-            </a>
-          </div>
+    <div class="col-md-6 col-lg-5">
+      <div class="card shadow rounded-4 overflow-hidden border-0 h-100 d-flex flex-column">
+        <!-- Thumbnail with Play Icon -->
+        <div class="position-relative" style="height: 200px; background-image: url('{{ asset('assets/yc/imgs/page/homepage1/event-09.jpg') }}'); background-size: cover; background-position: center;">
+          <!-- Play Button triggers modal -->
+          <a href="#" class="position-absolute top-50 start-50 translate-middle text-white text-decoration-none" data-bs-toggle="modal" data-bs-target="#videoModal1">
+            <i class="bi bi-play-circle-fill" style="font-size: 4rem;"></i>
+          </a>
+        </div>
 
-          <!-- Content Section -->
-          <div class="p-4 bg-white d-flex flex-column justify-content-between flex-grow-1">
-            <div>
-              <h6 class="fw-bold mb-2 text-dark">Lessons from the Trenches</h6>
-              <ul class="list-unstyled small text-muted mb-3">
-                <li class="mb-1"><i class="bi bi-mic-fill me-2 text-warning"></i> Kishorekumar</li>
-                <li class="mb-1"><i class="bi bi-calendar3 me-2 text-warning"></i> 20 Mar 2025</li>
-                <li class="mb-1"><i class="bi bi-list-task me-2 text-warning"></i> Duration</li>
-              </ul>
+        <!-- Content Section -->
+        <div class="p-4 bg-white d-flex flex-column justify-content-between flex-grow-1">
+          <div>
+            <h6 class="fw-bold mb-2 text-dark">Lessons from the Trenches</h6>
+            <ul class="list-unstyled small text-muted mb-3">
+              <li class="mb-1"><i class="bi bi-mic-fill me-2 text-warning"></i> Kishorekumar</li>
+              <li class="mb-1"><i class="bi bi-calendar3 me-2 text-warning"></i> 20 Mar 2025</li>
+              <li class="mb-1"><i class="bi bi-list-task me-2 text-warning"></i> Duration</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- Video Modal -->
+      <div class="modal fade" id="videoModal1" tabindex="-1" aria-labelledby="videoModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+          <div class="modal-content bg-dark">
+            <div class="modal-header border-0">
+              <button type="button" class="btn-close btn-close-white ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body p-0">
+              <video id="videoPlayer1" class="w-100" controls>
+                <source src="{{ url('https://www.w3schools.com/html/mov_bbb.mp4') }}" type="video/mp4">
+                Your browser does not support the video tag.
+              </video>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-md-6 col-lg-5">
-        <div class="card shadow rounded-4 overflow-hidden border-0 h-100 d-flex flex-column">
-          <!-- Thumbnail with Play Icon -->
-          <div class="position-relative" style="height: 200px; background-image: url('{{ asset('assets/yc/imgs/page/homepage1/event-09.jpg') }}'); background-size: cover; background-position: center;">
-            <a href="#" class="position-absolute top-50 start-50 translate-middle text-white text-decoration-none">
-              <i class="bi bi-play-circle-fill" style="font-size: 4rem;"></i>
-            </a>
-          </div>
+    </div>
+    <div class="col-md-6 col-lg-5">
+      <div class="card shadow rounded-4 overflow-hidden border-0 h-100 d-flex flex-column">
+        <!-- Thumbnail with Play Icon -->
+        <div class="position-relative" style="height: 200px; background-image: url('{{ asset('assets/yc/imgs/page/homepage1/event-09.jpg') }}'); background-size: cover; background-position: center;">
+          <!-- Play Button triggers modal -->
+          <a href="#" class="position-absolute top-50 start-50 translate-middle text-white text-decoration-none" data-bs-toggle="modal" data-bs-target="#videoModal2">
+            <i class="bi bi-play-circle-fill" style="font-size: 4rem;"></i>
+          </a>
+        </div>
 
-          <!-- Content Section -->
-          <div class="p-4 bg-white d-flex flex-column justify-content-between flex-grow-1">
-            <div>
-              <h6 class="fw-bold mb-2 text-dark">Lessons from the Trenches</h6>
-              <ul class="list-unstyled small text-muted mb-3">
-                <li class="mb-1"><i class="bi bi-mic-fill me-2 text-warning"></i> Kishorekumar</li>
-                <li class="mb-1"><i class="bi bi-calendar3 me-2 text-warning"></i> 20 Mar 2025</li>
-                <li class="mb-1"><i class="bi bi-list-task me-2 text-warning"></i> Duration</li>
-              </ul>
-            </div>
-            
+        <!-- Content Section -->
+        <div class="p-4 bg-white d-flex flex-column justify-content-between flex-grow-1">
+          <div>
+            <h6 class="fw-bold mb-2 text-dark">Lessons from the Trenches</h6>
+            <ul class="list-unstyled small text-muted mb-3">
+              <li class="mb-1"><i class="bi bi-mic-fill me-2 text-warning"></i> Kishorekumar</li>
+              <li class="mb-1"><i class="bi bi-calendar3 me-2 text-warning"></i> 20 Mar 2025</li>
+              <li class="mb-1"><i class="bi bi-list-task me-2 text-warning"></i> Duration</li>
+            </ul>
           </div>
         </div>
       </div>
-      
-
+      <!-- Video Modal -->
+      <div class="modal fade" id="videoModal2" tabindex="-1" aria-labelledby="videoModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+          <div class="modal-content bg-dark">
+            <div class="modal-header border-0">
+              <button type="button" class="btn-close btn-close-white ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body p-0">
+              <video id="videoPlayer2" class="w-100" controls>
+                <source src="{{ url('https://www.w3schools.com/html/mov_bbb.mp4') }}" type="video/mp4">
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
     <div class="row justify-content-center g- mt-100">
 
       
-      <div class="col-md-6 col-lg-5">
-        <div class="card shadow rounded-4 overflow-hidden border-0 h-100 d-flex flex-column">
-          <!-- Thumbnail with Play Icon -->
-          <div class="position-relative" style="height: 200px; background-image: url('{{ asset('assets/yc/imgs/page/homepage1/event-09.jpg') }}'); background-size: cover; background-position: center;">
-            <a href="#" class="position-absolute top-50 start-50 translate-middle text-white text-decoration-none">
-              <i class="bi bi-play-circle-fill" style="font-size: 4rem;"></i>
-            </a>
-          </div>
+    <div class="col-md-6 col-lg-5">
+      <div class="card shadow rounded-4 overflow-hidden border-0 h-100 d-flex flex-column">
+        <!-- Thumbnail with Play Icon -->
+        <div class="position-relative" style="height: 200px; background-image: url('{{ asset('assets/yc/imgs/page/homepage1/event-09.jpg') }}'); background-size: cover; background-position: center;">
+          <!-- Play Button triggers modal -->
+          <a href="#" class="position-absolute top-50 start-50 translate-middle text-white text-decoration-none" data-bs-toggle="modal" data-bs-target="#videoModal3">
+            <i class="bi bi-play-circle-fill" style="font-size: 4rem;"></i>
+          </a>
+        </div>
 
-          <!-- Content Section -->
-          <div class="p-4 bg-white d-flex flex-column justify-content-between flex-grow-1">
-            <div>
-              <h6 class="fw-bold mb-2 text-dark">Lessons from the Trenches</h6>
-              <ul class="list-unstyled small text-muted mb-3">
-                <li class="mb-1"><i class="bi bi-mic-fill me-2 text-warning"></i> Kishorekumar</li>
-                <li class="mb-1"><i class="bi bi-calendar3 me-2 text-warning"></i> 20 Mar 2025</li>
-                <li class="mb-1"><i class="bi bi-list-task me-2 text-warning"></i> Duration</li>
-              </ul>
-            </div>
-            
+        <!-- Content Section -->
+        <div class="p-4 bg-white d-flex flex-column justify-content-between flex-grow-1">
+          <div>
+            <h6 class="fw-bold mb-2 text-dark">Lessons from the Trenches</h6>
+            <ul class="list-unstyled small text-muted mb-3">
+              <li class="mb-1"><i class="bi bi-mic-fill me-2 text-warning"></i> Kishorekumar</li>
+              <li class="mb-1"><i class="bi bi-calendar3 me-2 text-warning"></i> 20 Mar 2025</li>
+              <li class="mb-1"><i class="bi bi-list-task me-2 text-warning"></i> Duration</li>
+            </ul>
           </div>
         </div>
       </div>
-      <div class="col-md-6 col-lg-5">
-        <div class="card shadow rounded-4 overflow-hidden border-0 h-100 d-flex flex-column">
-          <!-- Thumbnail with Play Icon -->
-          <div class="position-relative" style="height: 200px; background-image: url('{{ asset('assets/yc/imgs/page/homepage1/event-09.jpg') }}'); background-size: cover; background-position: center;">
-            <a href="#" class="position-absolute top-50 start-50 translate-middle text-white text-decoration-none">
-              <i class="bi bi-play-circle-fill" style="font-size: 4rem;"></i>
-            </a>
-          </div>
-
-          <!-- Content Section -->
-          <div class="p-4 bg-white d-flex flex-column justify-content-between flex-grow-1">
-            <div>
-              <h6 class="fw-bold mb-2 text-dark">Lessons from the Trenches</h6>
-              <ul class="list-unstyled small text-muted mb-3">
-                <li class="mb-1"><i class="bi bi-mic-fill me-2 text-warning"></i> Kishorekumar</li>
-                <li class="mb-1"><i class="bi bi-calendar3 me-2 text-warning"></i> 20 Mar 2025</li>
-                <li class="mb-1"><i class="bi bi-list-task me-2 text-warning"></i> Duration</li>
-              </ul>
+      <!-- Video Modal -->
+      <div class="modal fade" id="videoModal3" tabindex="-1" aria-labelledby="videoModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+          <div class="modal-content bg-dark">
+            <div class="modal-header border-0">
+              <button type="button" class="btn-close btn-close-white ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            
+            <div class="modal-body p-0">
+              <video id="videoPlayer3" class="w-100" controls>
+                <source src="{{ asset('assets/yc/videos/sample-video.mp4') }}" type="video/mp4">
+                Your browser does not support the video tag.
+              </video>
+            </div>
           </div>
         </div>
       </div>
-      
+    </div>
+    <div class="col-md-6 col-lg-5">
+      <div class="card shadow rounded-4 overflow-hidden border-0 h-100 d-flex flex-column">
+        <!-- Thumbnail with Play Icon -->
+        <div class="position-relative" style="height: 200px; background-image: url('{{ asset('assets/yc/imgs/page/homepage1/event-09.jpg') }}'); background-size: cover; background-position: center;">
+          <!-- Play Button triggers modal -->
+          <a href="#" class="position-absolute top-50 start-50 translate-middle text-white text-decoration-none" data-bs-toggle="modal" data-bs-target="#videoModal4">
+            <i class="bi bi-play-circle-fill" style="font-size: 4rem;"></i>
+          </a>
+        </div>
+
+        <!-- Content Section -->
+        <div class="p-4 bg-white d-flex flex-column justify-content-between flex-grow-1">
+          <div>
+            <h6 class="fw-bold mb-2 text-dark">Lessons from the Trenches</h6>
+            <ul class="list-unstyled small text-muted mb-3">
+              <li class="mb-1"><i class="bi bi-mic-fill me-2 text-warning"></i> Kishorekumar</li>
+              <li class="mb-1"><i class="bi bi-calendar3 me-2 text-warning"></i> 20 Mar 2025</li>
+              <li class="mb-1"><i class="bi bi-list-task me-2 text-warning"></i> Duration</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- Video Modal -->
+      <div class="modal fade" id="videoModal4" tabindex="-1" aria-labelledby="videoModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+          <div class="modal-content bg-dark">
+            <div class="modal-header border-0">
+              <button type="button" class="btn-close btn-close-white ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body p-0">
+              <video id="videoPlayer4" class="w-100" controls>
+                <source src="{{ asset('assets/yc/videos/sample-video.mp4') }}" type="video/mp4">
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
     </div>
   </div>
@@ -175,5 +237,16 @@
   @include('layouts.script')
 
 </body>
+<script>
+  const videoModal = document.getElementById('videoModal1');
+  const videoPlayer = document.getElementById('videoPlayer1');
+
+  videoModal.addEventListener('hidden.bs.modal', function () {
+    videoPlayer.pause();
+    videoPlayer.currentTime = 0;
+  });
+</script>
+
+
 
 </html>
